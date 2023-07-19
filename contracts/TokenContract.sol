f9b131ba4d49457c003566f469e38fb24677ce82
PoMaMa69 <saswatapatra15@gmail.com>
2023-07-19 17:43:39 +0530

    .env and comment lines added

--- contracts/TokenContract.sol
+++ contracts/TokenContract.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -10,14 +9,18 @@ contract TokenContract is ERC20Capped, ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
 
+    // Constructor for TokenContract
     constructor(uint256 cap, uint256 reward) ERC20("TokenContract", "TKC") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
+
+    // Event to emit when tokens are transferred
     event TokenTransferred(address indexed from, address indexed to, uint256 value);
 
-      function getTotalSupply() public view returns (uint256) {
+    // Function to get the total supply of tokens
+    function getTotalSupply() public view returns (uint256) {
         return totalSupply();
     }
 
@@ -26,6 +29,7 @@ contract TokenContract is ERC20Capped, ERC20Burnable {
         super._mint(account, amount);
     }
 
+    // Function to mint rewards for miners
     function _mintMinerReward() internal {
         _mint(block.coinbase, blockReward);
     }
@@ -37,26 +41,36 @@ contract TokenContract is ERC20Capped, ERC20Burnable {
         super._beforeTokenTransfer(from, to, value);
     }
 
+    // Function to set the block reward
     function setBlockReward(uint256 reward) public onlyOwner {
         blockReward = reward * (10 ** decimals());
     }
+
+    // Function to get the balance of tokens for a given address
     function getBalance(address account) public view returns (uint256) {
         return balanceOf(account);
     }
+
+    // Function to transfer tokens from the sender's address to another address
     function transferTokens(address to, uint256 amount) public {
         _transfer(msg.sender, to, amount);
         emit TokenTransferred(msg.sender, to, amount);
     }
+
+    // Function to approve a certain amount of tokens to be spent by another address
     function approveTokens(address spender, uint256 amount) public {
         _approve(msg.sender, spender, amount);
     }
-       function transferFromOneAddressToAnother(address from, address to, uint256 amount) public {
+
+    // Function to transfer tokens from one address to another on behalf of a given address
+    function transferFromOneAddressToAnother(address from, address to, uint256 amount) public {
         transferFrom(from, to, amount);
         emit TokenTransferred(from, to, amount);
     }
+
+    // Modifier to ensure that only the owner can call certain functions
     modifier onlyOwner {
         require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 }
-
